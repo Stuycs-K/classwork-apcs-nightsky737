c0ba83ad8bb16a0acdd1c90880fdbfcfe1501cfb
Jessica Seto <jseto737@gmail.com>
2024-09-27 10:52:41 -0400

    Added copy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -3,11 +3,21 @@ class MyArrays{
     int[] testArr = new int[]{8, 2, 1 , 3};
     int[] testArr2 = new int[]{};
 
-    System.out.println(arrayToString(testArr));
-    System.out.println(arrayToString(testArr2));
+    int[] testCopy = returnCopy(testArr);
+    System.out.println("Do the memory Addresses equal? " + (testCopy == testArr));
+    System.out.println("Original: " + arrayToString(testArr));
+    System.out.println("Copied: " + arrayToString(testCopy));
   }
 
-   static String arrayToString(int[] nums){
+ public static int[] returnCopy(int[]ary){
+   int[] newArr = new int[ary.length];
+   for(int i = 0; i < ary.length; i++){
+     newArr[i] = ary[i];
+   }
+   return newArr;
+ }
+
+public static String arrayToString(int[] nums){
     String out = "[";
     for(int i = 0; i < nums.length; i++){
       if(i == nums.length - 1){
