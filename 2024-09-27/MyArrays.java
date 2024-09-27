c15a11adea478491b5933fdc897b00e56579fbd8
Jessica Seto <jseto737@gmail.com>
2024-09-27 11:01:18 -0400

    Concat not working for empty array

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -2,11 +2,26 @@ class MyArrays{
   public static void main(String[] args){
     int[] testArr = new int[]{8, 2, 1 , 3};
     int[] testArr2 = new int[]{};
+    int[] testArr3 = {2, 4, 2, 2, 2, 9};
 
     int[] testCopy = returnCopy(testArr);
     System.out.println("Do the memory Addresses equal? " + (testCopy == testArr));
     System.out.println("Original: " + arrayToString(testArr));
     System.out.println("Copied: " + arrayToString(testCopy));
+
+    testCopy = returnCopy(testArr2);
+    System.out.println("Do the memory Addresses equal? " + (testCopy == testArr2));
+    System.out.println("Original: " + arrayToString(testArr2));
+    System.out.println("Copied: " + arrayToString(testCopy));
+
+    testCopy = returnCopy(testArr3);
+    System.out.println("Do the memory Addresses equal? " + (testCopy == testArr3));
+    System.out.println("Original: " + arrayToString(testArr3));
+    System.out.println("Copied: " + arrayToString(testCopy));
+
+
+    System.out.println("Concatenated " + arrayToString(testArr) + " and " + arrayToString(testArr2));
+    System.out.println(arrayToString(concatArray(testArr, testArr2)));
   }
 
  public static int[] returnCopy(int[]ary){
@@ -16,6 +31,21 @@ class MyArrays{
    }
    return newArr;
  }
+
+
+public static int[] concatArray(int[]ary1,int[]ary2){
+    int[] out = new int[ary1.length + ary2.length];
+    for(int i = 0; i < ary1.length; i++){
+      out[i] = ary1[i];
+    }
+    for(int i = ary1.length - 1; i < out.length; i++){
+      out[i] = ary2[i - ary1.length + 1];
+    }
+
+    return out;
+  }
+
+
 
 public static String arrayToString(int[] nums){
     String out = "[";
