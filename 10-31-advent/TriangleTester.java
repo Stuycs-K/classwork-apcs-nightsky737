7d4ee98a74e300d590096d778d8755ee063556d0
Jessica Seto <jseto737@gmail.com>
2024-11-04 09:41:29 -0600

    added and tested the helper function to determine if triangle is valid

--- 10-31-advent/TriangleTester.java
+++ 10-31-advent/TriangleTester.java
@@ -6,22 +6,37 @@ import java.util.Scanner;
 
 public class TriangleTester{
     public static int countTrianglesA(String filename){
-      File file = new File(filename);
-      Scanner triangles = new Scanner(file);
-  
+
       int a = 0;
       int b = 0;
       int c = 0;
       int count = 0;
-      while(triangles.hasNextLine()){
+      try{
+      File file = new File(filename);
+      Scanner triangles = new Scanner(file);
+
+      while(triangles.hasNextLine() && triangles.hasNextInt()){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
         a = triangles.nextInt();
         b = triangles.nextInt();
         c = triangles.nextInt();
-        if(a + b >= c && a + c >= b && b + c >= a){
+        if(isValid(a, b, c)){
           count++;
         }
       }
+      triangles.close();
+
+  }
+      catch(FileNotFoundException ex){
+        System.out.println("File not found");
+      }
+
     return count;
     }
+    public static boolean isValid(int a, int b, int c){
+      return (a + b > c && a + c > b && b + c > a);
+    }
+
   }
-  
