cf5dc358350b0f5906f290924b8dbe4acd1a1b92
Jessica Seto <jseto737@gmail.com>
2024-11-18 16:55:44 -0500

    wrote draft of solution

--- 2024-11-18-advent2016/day1.java
+++ 2024-11-18-advent2016/day1.java
@@ -8,20 +8,27 @@ public class day1{
     try{
     File file = new File("day1in");
     Scanner inScan = new Scanner(file);
-
+    int direction = 0; //0 is North,
     String[] intext = inScan.nextLine().split(", ");
-    int L = 0;
-    int R = 0;
+    int x, y = 0;
     for(int i = 0; i < intext.length; i++){
-      if(intext[i].charAt(0) == 'L'){
-        L += Integer.parseInt(intext[i].subString(1));
+      if(intext[i].charAt(0) == 'R'){
+        direction += 1;
       }else{
-
+        direction -= 1;
+      }
+      if(direction % 4 == 0){
+        y += Integer.parseInt(intext[i].substring(1));
+      }else if(direction % 4 == 2){
+        y -= Integer.parseInt(intext[i].substring(1));
+      }else if(direction % 4 == 1){
+        x += Integer.parseInt(intext[i].substring(1));
+      }else{
+        x -= Integer.parseInt(intext[i].substring(1));
       }
     }
-
-    System.out.println(Arrays.toString(intext));
-    System.out.println(L);
+    double distance = Math.sqrt(x * x + y * y);
+    Sytem.out.println(distance);
   }
     catch(FileNotFoundException e){
       System.out.println("your file bad");
