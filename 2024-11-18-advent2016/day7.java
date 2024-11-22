64caf83a2a86a6af76d7f14db01317f4fa0962b0
Jessica Seto <jseto737@gmail.com>
2024-11-21 22:46:07 -0500

    added day7

--- 2024-11-18-advent2016/day7.java
+++ 2024-11-18-advent2016/day7.java
@@ -9,6 +9,9 @@ public class day7 {
         && str.charAt(1) == str.charAt(2) 
         && str.charAt(3) != str.charAt(2) );
     }
+    public static boolean aba(String str){
+      return(str.charAt(0) == str.charAt(2) && (str.charAt(2) != str.charAt(1)));
+    }
     public static void main(String[] args){
  try{
       File file = new File("day7in.txt");
@@ -20,26 +23,59 @@ public class day7 {
         boolean insquare = false;
         boolean outsquare = false;
         bracket = false;
-        for(int i = 0; i <= line.length() - 4; i++){
-          if(line.charAt(i) == '['){
-            bracket = true;
-            continue;
-          }else if(line.charAt(i) == ']'){
-            bracket = false;
-            continue;
-          }
-          if(check(line.substring(i, i+4))){
-            if(bracket){
-                insquare = true;
-            }else{
-                outsquare = true;
+        ArrayList<String> nobracks = new ArrayList<String>();
+        ArrayList<String> bracks = new ArrayList<String>();
+
+        while(line.indexOf("[") != -1){
+          int start = line.indexOf("[");
+          int end = line.indexOf("]");
+          String nobrackin = line.substring(0, start);
+          for(int j = 0; j < nobrackin.length() - 2; j++){
+            if(aba(nobrackin.substring(j, j+3))){
+              nobracks.add(nobrackin.substring(j, j+3));
             }
           }
+          String bracksin = line.substring(start + 1, end);
+          for(int j = 0; j < bracksin.length() - 2; j++){
+            if(aba(bracksin.substring(j, j+3))){
+              bracks.add(bracksin.substring(j, j+3));
+            }
+          }
+          line = line.substring(end + 1);
+        }
+        for(int j = 0; j < line.length() - 2; j++){
+          if(aba(line.substring(j, j+3))){
+            nobracks.add(line.substring(j, j+3));
+          }
+        }
 
+       for(int i = 0; i < nobracks.size(); i++){
+        String target = nobracks.get(i);
+        if(bracks.contains("" + target.charAt(1) + target.charAt(0) + target.charAt(1))){
+          total++;
+          break;
         }
-        if(!insquare && outsquare){
-            total+= 1;
-        }
+       }
+        // for(int i = 0; i <= line.length() - 4; i++){
+        //   if(line.charAt(i) == '['){
+        //     bracket = true;
+        //     continue;
+        //   }else if(line.charAt(i) == ']'){
+        //     bracket = false;
+        //     continue;
+        //   }
+        //   if(check(line.substring(i, i+4))){
+        //     if(bracket){
+        //         insquare = true;
+        //     }else{
+        //         outsquare = true;
+        //     }
+        //   }
+
+        // }
+        // if(!insquare && outsquare){
+        //     total+= 1;
+        // }
       }
       System.out.println(total);
 
