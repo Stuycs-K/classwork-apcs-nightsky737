d3115fbc95cb29b17578a8846061ce3e5277f170
nightsky737 <jseto737@gmail.com>
2024-12-03 22:20:43 -0500

    very colorful line that kind of looks like holiday lights

--- 2024-12-3-colors/ColorDemo.java
+++ 2024-12-3-colors/ColorDemo.java
@@ -4,21 +4,20 @@ class ColorDemo{
   public static final String SHOW_CURSOR =  "\u001b[?25h";
 
   public static void main(String[] args){
-    String[] ary = new String[10];
-    for(int i = 0; i < ary.length; i++){
-      ary[i] = ".";
+    String str = "...----````----..";
+    int[] colores = {90, 91, 92, 93, 94, 95, 96, 97,98,98,97,96,95,94,93,92,91,90};
+    int color = 0;
+    for(int repeats = 0; repeats<10; repeats++){
+      for(int i = 0; i < str.length(); i++){
+        System.out.print(str.charAt(i));
+        System.out.print("\u001b["+colores[color % colores.length]+"m");
+        color++;
+        sleep(15);
+
+      }  
     }
-    for(int i = 0; i < ary.length; i++){
-      for(int j = 0; j < ary.length; j++){
-        if(i == j){
-          System.out.print("i");
-        }else{
-          System.out.print(",");
-        }
-        sleep(10);
-      }
-      System.out.print(CLEAR_SCREEN);
-    }
+
+  
 
     System.out.print("\u001b[0m");
   }
