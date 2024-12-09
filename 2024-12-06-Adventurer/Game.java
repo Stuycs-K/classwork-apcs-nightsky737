678dcbe6d30453e35c6b0ae8bfbd7ddbb1b16762
Jessica Seto <jseto737@gmail.com>
2024-12-09 10:44:22 -0500

    added codewarrior

---
+++ 2024-12-06-Adventurer/Game.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Game{
+  public static void main(String[] args) {
+    //do this once
+    Scanner userInput = new Scanner(System.in);
+
+    //You can do the rest many times:
+
+    System.out.println("Enter username");
+    //Read one line of user input
+    String userName = userInput.nextLine();
+    //Do something with the input
+    System.out.println("Username is: " + userName);
+  }
+}
